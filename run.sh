2412a75fb288379c5462fb0d777180aa72a52ed7
Yi Chun Lin <yilin@made-f0Br5s6.clusters.42paris.fr>
2024-12-30 20:11:18 +0100

    trying to set bash script to ignore libc leak

--- run.sh
+++ run.sh
@@ -2,4 +2,17 @@
 
 dir=$(pwd)
 
-valgrind -q --track-fds=yes --leak-check=full --show-leak-kinds=all --trace-children=yes --show-reachable=yes --suppressions=./readline.supp ./minishell
+#valgrind -q --track-fds=yes --leak-check=full --show-leak-kinds=all --trace-children=yes --show-reachable=yes --suppressions=./readline.supp ./minishell
+
+# Use a comprehensive suppression file that includes readline and libc suppressions
+SUPPRESSION_FILE="$dir/readline.supp"
+
+# Run Valgrind with the updated suppression file
+valgrind -q \
+    --track-fds=yes \
+    --leak-check=full \
+    --show-leak-kinds=all \
+    --trace-children=yes \
+    --show-reachable=yes \
+    --suppressions="$SUPPRESSION_FILE" \
+    ./minishell
