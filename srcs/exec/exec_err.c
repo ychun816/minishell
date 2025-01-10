370e85e414280f079c58be612b39c55356f179fe
Vanessa Rodrigues <varodrig@bess-f2r3s5.clusters.42paris.fr>
2025-01-10 18:47:51 +0100

    fixed permission denied

--- srcs/exec/exec_err.c
+++ srcs/exec/exec_err.c
@@ -6,7 +6,7 @@
 /*   By: varodrig <varodrig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 14:12:05 by varodrig          #+#    #+#             */
-/*   Updated: 2025/01/10 18:20:30 by varodrig         ###   ########.fr       */
+/*   Updated: 2025/01/10 18:46:32 by varodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,6 +67,9 @@ void	exe_err_coredump(int pid)
 	exe_close(&fd_backup);
 }
 
+// stat(path, &stats) !stores information abour path in stat
+// returns -1 if no exist or permission denied
+// S_ISDIR(stats.st_mode) == 1 if its directory
 void	err_execve(char *path, int err_no)
 {
 	int			fd_tmp;
@@ -78,12 +81,12 @@ void	err_execve(char *path, int err_no)
 		printf("%s: %s: command not found\n", PROMPT_NAME, path);
 	else if (err_no == ENOENT)
 		printf("%s: %s: No such file or directory\n", PROMPT_NAME, path);
-	else if (err_no == EACCES)
-		printf("%s: %s: Permission denied\n", PROMPT_NAME, path);
 	else if (stat(path, &stats) != -1)
 	{
 		if (S_ISDIR(stats.st_mode) == 1)
 			printf("%s: %s: Is a directory\n", PROMPT_NAME, path);
+		else if (err_no == EACCES)
+			printf("%s: %s: Permission denied\n", PROMPT_NAME, path);
 		else
 			printf("%s: %s: %s\n", PROMPT_NAME, path, strerror(err_no));
 	}
