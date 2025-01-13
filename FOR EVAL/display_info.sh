fe87f6beec64b51aa90c9281d330f467d4eb811c
Yi Chun Lin <yilin@bess-f4r5s6.clusters.42paris.fr>
2025-01-13 17:22:47 +0100

    rearrange minishell file

--- FOR EVAL/display_info.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-dir=$(pwd)
-
-valgrind --track-fds=yes --leak-check=full --show-leak-kinds=all --trace-children=yes --show-reachable=yes --suppressions=./readline.supp ./minishell
-
--- FOR EVAL/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-dir=$(pwd)
-
-valgrind -q --track-fds=yes --leak-check=full --show-leak-kinds=all --trace-children=yes --show-reachable=yes --suppressions=./readline.supp ./minishell
-#valgrind -q --track-fds=yes --leak-check=full --show-leak-kinds=all --trace-children=yes --show-reachable=yes --suppressions=    ./libc_leak.supp ./minishell
